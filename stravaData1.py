b4df98682aa1947c4f74d8a86042f1ff866c9b65
Sahil Singh <sahilsingh08062001@gmail.com>
2021-08-27 21:01:19 +0530

    updates

--- stravaData1.py
+++ stravaData1.py
@@ -52,25 +52,34 @@ def table():
     nameL = []
     distL = []
     roundsL = []
+    rankL = []
 
     for i in range(len(nameList)):
         nameL.append(nameList[i][0])
         distL.append(distList[i][1])
         roundsL.append(nameList[i][1])
+        rankL.append(i+1)
 
     dataDict = {'Name': nameL, 'Distance': distL, 'Rounds': roundsL}
 
     df = pd.DataFrame(dataDict)
-
+    print(df)
     df = df.sort_values(by=['Distance'], ascending=False)
+    nameL1 = df['Name'].to_list()
+    distL1 = df['Distance'].to_list()
+    roundsL1 = df['Rounds'].to_list()
+    print(nameL1)
+    dataDict1 = {'Name': nameL1, 'Distance': distL1, 'Rounds': roundsL1}
+    df1 = pd.DataFrame(dataDict1)
+    print(df1)
+    df1.index += 1
 
-    print(type(df.to_html()))
 
-    htmlTable = df.to_html()
+    htmlTable = df1.to_html()
     htmlTable = htmlTable[200:]
     htmlTable = htmlTable[:-17]
     # htmlTable.replace("</th>", "</td>")
     # htmlTable.replace("<th>", "<td>")
-    print(df.to_html())
+    # print(htmlTable)
 
     return render_template('index.html', data=htmlTable, heading="FIT INDIA 2.0 Cycling")
